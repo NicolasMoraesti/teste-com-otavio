b1c8d025d77ab1a25ed1598991c242a220cc1818
NicolasMoraesti <ncmoraees@gmail.com>
2025-05-13 12:06:42 -0300

    Remoçao-email

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
     var nome = $('#nome_usuario').val();
     var idade = $('#idade_usuario').val();
-    var email = $('#email_usuario').val();
+    
 
-    alert(`Seu nome é: ${nome} sua idade é: ${idade} e seu E-mail é: ${email}`);
+    alert(`Seu nome é: ${nome} sua idade é: ${idade}`);
   });
 });
