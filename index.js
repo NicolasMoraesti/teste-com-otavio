f780a6dbacc293d57e8ad8b2fd56626002cd5325
NicolasMoraesti <ncmoraees@gmail.com>
2025-05-16 11:39:05 -0300

    correçao no email e adiçao de alert

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
 
     var nome = $('#nome_usuario').val();
     var idade = $('#idade_usuario').val();
+    var email = $('#email_usuario').val();
     
 
-    alert(`Seu nome é: ${nome} sua idade é: ${idade}`);
+    alert(`Seu nome é: ${nome} sua idade é: ${idade} Seu Email é: ${email}`);
   });
 });
